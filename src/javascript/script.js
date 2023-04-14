fd15f39969556526b3dc43af5de0e120737a9a42
Jhonel Villanueva <vvjhonel@gmail.com>
2023-04-15 00:25:37 +0800

    ref: Update init to displayCurrentBoard

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,3 +1,24 @@
+const gameBoard = (() => {
+  const board = ['', '', '', '', '', '', '', '', ''];
+  const cells = document.querySelectorAll('.board-cell');
+
+  const displayCurrentBoard = () => {
+    cells.forEach((cell, index) => {
+      const updatedCell = cell;
+      updatedCell.textContent = board[index];
+    });
+  };
+
+  const updateBoard = (move, token) => {
+    board[move] = token;
+  };
+
+  return {
+    displayCurrentBoard,
+    updateBoard,
+  };
+})();
+
 const Player = (name, token) => {
   const getName = () => name;
   const getToken = () => token;
@@ -8,31 +29,19 @@ const Player = (name, token) => {
   };
 };
 
-const gameBoard = (() => {
-  const board = ['', '', '', '', '', '', '', '', ''];
-  const cells = document.querySelectorAll('.board-cell');
+const gameController = (() => {
+  let gameOver = false;
+  const playerOne = Player('Player 1', 'O');
+  const playerTwo = Player('Player 2', 'X');
+  const currentPlayer = playerOne;
 
-  const updateBoard = (move, token) => {
-    board[move] = token;
-
-    cells.forEach((cell, index) => {
-      const updatedCell = cell;
-      updatedCell.textContent = board[index];
-    });
+  const play = () => {
+    gameBoard.displayCurrentBoard();
   };
 
-  const init = () => {
-    cells.forEach((cell, index) => {
-      const updatedCell = cell;
-      updatedCell.textContent = board[index];
-
-      updatedCell.addEventListener('click', (e) => {
-        e.preventDefault();
-
-        updateBoard(index, 'O');
-      });
-    });
+  return {
+    play,
   };
-
-  init();
 })();
+
+gameController.play();
